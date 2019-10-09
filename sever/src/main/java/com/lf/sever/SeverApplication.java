3403f233462d26ff5df06168d82ac851ef630455
“ <lfcreater@outlook.com>
2019-10-09 16:35:49 +0800

    springcloudDemo 服务注册与服务发现

---
+++ sever/src/main/java/com/lf/sever/SeverApplication.java
@@ -0,0 +1,15 @@
+package com.lf.sever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class SeverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeverApplication.class, args);
+    }
+
+}
